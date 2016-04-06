9f5b8c1cf3232cfd74b4782ff474fbd90a42f940
simmer <xushujun_sunny@163.com>
2016-04-07 01:44:48 +0800

    publish task-18

---
+++ common/js/utils.js
@@ -0,0 +1,68 @@
+/*
+*******  工具函数集合
+*/
+(function(root, name, factory) {
+    if(typeof define === 'function' && !!define.amd) {// amd
+        define(factory);
+    } else if (typeof exprots === 'object') { // commonJS
+        module.exports = factory();
+    } else {
+        //console.log('$' === name);
+        //root.$ = factory(); // global module
+        //console.log(root.name === root['$']);
+        root.$ = factory();
+    }
+}(window, '$' , function() {
+    var $ = function() {
+        return new $.prototype.init(arguments);
+    };
+    $.prototype = {
+        init: function( ) {
+            //
+        }
+    };
+    $.prototype = $.prototype.init.prototype; // change prototype
+
+    $.eventUtil = (function(element) {
+        var ret;
+        if(element.addEventListener) {
+            ret =  {
+                on: function(element, type, handler) {
+                    element.addEventListener(type, handler, false);
+                },
+                un: function(element, type, handler) {
+                    element.removeEventListener(type, handler, false);
+                }
+            };
+        } else if(element.attachEvent) {
+            ret = {
+                on: function(element, type, handler) {
+                    element.attachEvent('on'+ type, handler);
+                },
+                un: function(element, type, handler) {
+                    element.detachEvent('on' + type, handler);
+                }
+            };
+        } else {
+            ret = {
+                on: function(element, type, handler) {
+                    element['on' + type] = handler;
+                },
+                un: function(element, type, handler) {
+                    element['on' + type] = null;
+                }
+            };
+        }
+        ret.delegate = function(tagName, parentElement, type, handler) {
+            ret.on(parentElement, type, function(e) {
+                e = e || window.event;
+                var element = e.target || e.srcElement;
+                if(element.nodeType ===1 && element.tagName === tagName.toUpperCase()) {
+                    handler(e);
+                }
+            });
+        }
+        return ret;
+    })(document);
+    return $;
+}));
